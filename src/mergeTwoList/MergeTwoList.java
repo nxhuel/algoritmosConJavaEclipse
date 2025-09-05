793b1f40c6d326503c6546937fe57df82ae28472
nxhuel <nahueltisera03@gmail.com>
2025-09-05 17:10:54 -0300

    algoritmos: ejercicio prefijo comun mas largo finalizado, falta integrar unit test

---
+++ src/mergeTwoList/MergeTwoList.java
@@ -0,0 +1,11 @@
+package mergeTwoList;
+
+public class MergeTwoList {
+
+
+	public ListNode mergeTwoList(ListNode list1, ListNode list2) {
+		ListNode listNode = new ListNode(21);
+		
+		return listNode;
+	}
+}
